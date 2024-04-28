cd9f748078fe456b97c0106e6fc7bbd7f0708942
jhonalexander019 <jhon.roa@unillanos.edu.co>
2024-04-28 00:02:09 -0500

    Skills terminadas, mejora de galeria

--- JS/Carousel.js
+++ JS/Carousel.js
@@ -4,10 +4,12 @@ const iconList2 = document.getElementById('icon-list2');
 
 
 // Array con los nombres de los archivos wepb (sin la extensión)
-const svgIcons = ['gitHub', 'tailwind', 'react', 'npm', 'bootstrap', 'JS', 'ts', 'angular', 'HTML', 'nextjs', 'nodeJS', 'java', 'springboot', 'kotlin', 'css'];
+const Icons = ['github', 'tailwind', 'react', 'npm', 'bootstrap', 'JS', 'ts', 'angular', 'HTML', 'nextjs', 'nodeJS', 'java', 'springboot', 'kotlin', 'css'];
+const Skills = ['tailwind', 'npm', 'bootstrap', 'JS', 'ts', 'HTML', 'nodeJS', 'java', 'kotlin', 'css'];
+
 
 // Iterar sobre los nombres de los archivos wepb
-svgIcons.forEach(iconName => {
+Icons.forEach(iconName => {
     // Crear un elemento li para cada icono en la primera lista
     const li1 = document.createElement('li');
 
@@ -18,29 +20,33 @@ svgIcons.forEach(iconName => {
     img1.loading = 'lazy';
 
     const span1 = document.createElement('span');
-    span1.textContent = iconName;
+    const strong1 = document.createElement('strong');
+    strong1.textContent = iconName; // Agregar el texto al strong
     span1.classList.add('tooltip');
+    span1.appendChild(strong1); // Agregar el strong como hijo del span
+    
+    
 
     li1.appendChild(img1);
     li1.appendChild(span1);
 
     iconList.appendChild(li1);
 
-    // Crear un elemento li para cada icono en la segunda lista
-    const li2 = document.createElement('li');
+    
+});
 
-    const img2 = document.createElement('img');
-    img2.src = `./Img/Icons/${iconName}.webp`;
-    img2.alt = `Icono ${iconName}`;
-    img2.classList.add('icon-wepb');
-    img2.loading = 'lazy';
+Skills.forEach(skill => {
+// Crear un elemento li para cada icono en la segunda lista
+const li2 = document.createElement('li');
 
-    const span2 = document.createElement('span');
-    span2.textContent = iconName;
-    span2.classList.add('tooltip');
+const img2 = document.createElement('img');
+img2.src = `./Img/Icons/${skill}.webp`;
+img2.alt = `Icono ${skill}`;
+img2.classList.add('icon-wepb');
+img2.loading = 'lazy';
 
-    li2.appendChild(img2);
-    li2.appendChild(span2);
+li2.appendChild(img2);
 
-    iconList2.appendChild(li2);
+iconList2.appendChild(li2);
+
 });
